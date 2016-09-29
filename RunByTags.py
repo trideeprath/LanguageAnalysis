baf34c26a7a3916f6d2fef50b2fca4bf33ae5ada
Anupam Panwar <anupam.p.iitd@gmail.com>
2016-09-29 00:52:03 -0700

    File name by Tag for question Collection

---
+++ RunByTags.py
@@ -0,0 +1,18 @@
+import argparse
+
+__Author__ = "Anupam Panwar"
+
+__Date__ = 9 / 29 / 16
+
+import run as run
+
+if __name__ == "__main__":
+    argparser = argparse.ArgumentParser()
+    argparser.add_argument('TAG', action='store', help='Tag')
+    argparser.add_argument('-q', dest='QUES', default=True, help='Fetch Ques')
+    argparser.add_argument('-k', dest='KEYPHRASE', default=False, help='Fetch keyphrase')
+    argparser.add_argument('-o', dest='OUTPUT_FILE', default='csv', help='Output file name ')
+    args = argparser.parse_args()
+
+    run(args.TAG, args.QUES, args.KEYPHRASE, args.OUTPUT_FILE)
+
